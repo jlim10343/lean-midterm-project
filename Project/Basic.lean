23b4ed18345ccf887ca559cbd82cc2923d12f5d3
George Zhou <GZhou7819@gmail.com>
2023-11-05 16:48:42 -0500

    finished defining functions

--- Project/Basic.lean
+++ Project/Basic.lean
@@ -8,4 +8,26 @@ inductive Tree (α : Type u) where
   | Empty : Tree α
   | Node : Tree α → α → Tree α → Tree α
 
+def append {α : Type u} (l : List α) (r : List α) : List α :=
+  match l with
+    | List.Nil => r
+    | List.Cons x xs => List.Cons x (append xs r)
+
+infixl:70 " @ " => append
+
+def map {α β : Type u} (f : α → β) (l : List α) : List β :=
+  match l with
+    | List.Nil => List.Nil
+    | List.Cons x xs => List.Cons (f x) (map f xs)
+
+def treeMap {α β : Type u} (f : α → β) (t : Tree α) : Tree β :=
+  match t with
+    | Tree.Empty => Tree.Empty
+    | Tree.Node L x R => Tree.Node (treeMap f L) (f x) (treeMap f R)
+
+def inord {α : Type u} (t : Tree α) : List α :=
+  match t with
+    | Tree.Empty => List.Nil
+    | Tree.Node L x R => (inord L) @ (List.Cons x (inord R))
+
 end fifteen_one_fifty

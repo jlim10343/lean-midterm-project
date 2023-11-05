86e216deda113f35abd092258e6952fb0ee74527
George Zhou <GZhou7819@gmail.com>
2023-11-05 16:40:47 -0500

    added inductive datatypes

--- Project/Basic.lean
+++ Project/Basic.lean
@@ -1,1 +1,11 @@
-def hello := "world"
+namespace fifteen_one_fifty
+
+inductive List (α : Type u) where
+  | Nil : List α
+  | Cons : α → List α → List α
+
+inductive Tree (α : Type u) where
+  | Empty : Tree α
+  | Node : Tree α → α → Tree α → Tree α
+
+end fifteen_one_fifty

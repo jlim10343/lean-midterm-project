fab6082ecb63744497b7e3a0267316d418b69911
George Zhou <GZhou7819@gmail.com>
2023-11-11 16:01:17 -0500

    finished map append yayy

--- Project/project.lean
+++ Project/project.lean
@@ -13,7 +13,7 @@ def append {α : Type u} (l : List α) (r : List α) : List α :=
     | List.Nil => r
     | List.Cons x xs => List.Cons x (append xs r)
 
-infixl:70 " @ " => append
+infixl:69 " @ " => append
 
 def map {α β : Type u} (f : α → β) (l : List α) : List β :=
   match l with
@@ -29,5 +29,22 @@ def inord {α : Type u} (t : Tree α) : List α :=
   match t with
     | Tree.Empty => List.Nil
     | Tree.Node L x R => (inord L) @ (List.Cons x (inord R))
+
+theorem mapAppend {α β : Type u} (A : List α) (B : List α) (f : α → β) :
+  map f (A @ B) = (map f A) @ (map f B) := by
+    induction A with
+    | Nil =>
+        calc
+          map f (List.Nil @ B) = map f B := by rfl
+          _                    = List.Nil @ (map f B) := by rfl
+          _                    = (map f List.Nil) @ (map f B) := by rfl
+    | Cons x xs ih =>
+        calc
+          map f ((List.Cons x xs) @ B) = map f (List.Cons x (xs @ B)) := by rfl
+          _                            = List.Cons (f x) (map f (xs @ B)) := by rfl
+          _                            = List.Cons (f x) ((map f xs) @ (map f B)) := by rw [ih]
+          _                            = (List.Cons (f x) (map f xs)) @ (map f B) := by rfl
+          _                            = (map f (List.Cons x xs)) @ (map f B) := by rfl
+
 
 end structural_datatypes

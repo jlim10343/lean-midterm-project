7a4e41698c2b208796d4fb50c6a5a60174c2aa1a
jlim10343 <aiwen.joy.lim@gmail.com>
2023-11-12 16:32:38 -0500

    Starting extension

--- Project/project.lean
+++ Project/project.lean
@@ -31,6 +31,38 @@ def inord {α : Type u} (t : Tree α) : List α :=
     | Tree.Empty => List.Nil
     | Tree.Node L x R => (inord L) @ (x::(inord R))
 
+def leaves {α : Type u} (t : Tree α) : List α :=
+  match t with
+    | Tree.Empty => List.Nil
+    | Tree.Node L x R =>
+        match L with
+        | Tree.Node L' x' R' => (leaves L) @ (leaves R)
+        | Tree.Empty =>
+            match R with
+            | Tree.Node L'' x'' R'' => leaves R
+            | Tree.Empty => x::List.Nil
+
+def trim {α : Type u} (t : Tree α) : Tree α :=
+  match t with
+  | Tree.Empty => Tree.Empty
+  | Tree.Node L x R =>
+      match L with
+        | Tree.Node L' x' R' => Tree.Node (trim L) x (trim R)
+        | Tree.Empty =>
+            match R with
+            | Tree.Node L'' x'' R'' => Tree.Node (Tree.Empty) x (trim R)
+            | Tree.Empty => Tree.Empty
+
+def size {α : Type u} (t : Tree α) : Nat :=
+  match t with
+  | Tree.Empty => 0
+  | Tree.Node L x R => 1 + size L + size R
+
+def len {α : Type u} (l : List α) : Nat :=
+  match l with
+  | List.Nil => 0
+  | List.Cons x xs => 1 + len xs
+
 theorem mapAppend {α β : Type u} (A : List α) (B : List α) (f : α → β) :
   map f (A @ B) = (map f A) @ (map f B) := by
     induction A with
@@ -64,6 +96,25 @@ theorem inordMap {α β : Type u} (T : Tree α) (f : α → β) :
           _                                   = (map f (inord L)) @ (map f (x::(inord R))) := by rw [map]
           _                                   = map f ((inord L) @ (x::(inord R))) := by rw [← mapAppend]
           _                                   = map f (inord (Tree.Node L x R)) := by rw [inord]
+
+
+theorem lenAppend {α : Type u} (l1 : List α) (l2 : List α) :
+  len (l1 @ l2) = len l1 + len l2 := by
+  induction l1 with
+  | Nil =>
+      calc
+        len (List.Nil @ l2) = len l2 := by rfl
+        _                   = 0 + len l2 := by sorry
+        _                   = len List.Nil + len l2 := by rfl
+  | Cons x xs ih =>
+      calc
+        len ((x::xs) @ l2) = len (x::(xs @ l2)) := by rw [append]
+        _                  = 1 + len (xs @ l2) := by rw [len]
+        _                  = 1 + len xs + len l2 := by sorry --ih
+        _                  = len (x::xs) + len l2 := by rw [len]
+
+theorem trimSize {α : Type u} (T : Tree α) :
+  size (trim T) + len (leaves T) = size T := by sorry
 
 inductive Rose (α : Type u) where
   | Root : α → List (Rose α) → Rose α

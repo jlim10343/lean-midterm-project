e0702c40cc88e4d6217f4c5f25017b67f0619d8b
George Zhou <GZhou7819@gmail.com>
2023-11-28 16:51:56 -0500

    finished writing up ri statement

--- Project/final-ri.lean
+++ Project/final-ri.lean
@@ -2,34 +2,55 @@ import Project.midterm
 
 namespace structural_datatypes
 
-inductive Rose (α : Type) where
-  | Root : α → List (Rose α) → Rose α
+inductive Option (α : Type) where
+  | NONE : Option α
+  | SOME : α → Option α
 
-def treePreord {α : Type} (T : Tree α) : List α :=
-  match T with
-    | Tree.Empty => List.Nil
-    | Tree.Node L x R => x::((treePreord L) @ (treePreord R))
+structure Queue (α : Type) where
+  t : Type α
 
-def foldl {α β : Type} (g : α → β → β) (z : β) (L : List α) : β :=
-  match L with
-    | List.Nil => z
-    | List.Cons x xs => fold g (g x z) xs
+  empty : t
+  enq (x : α) (Q : t) : t
+  deq (q : t) : Option α
 
--- def sizeRose {α : Type} (R : Rose α) : Nat :=
---   match R with
---     | Rose.Root x rs => 1 + (fold (Nat.add) 0 (map sizeRose rs))
+def QueueList : Queue α where
+  t := List α
 
-mutual
-def rosePreord {α : Type} (R : Rose α) : List α :=
-  match R with
-    | Rose.Root x rs => x::(mapRosePreord rs)
-def mapRosePreord {α : Type} (rs : List (Rose α)) : List α :=
-  match rs with
-    | List.Nil => List.Nil
-    | List.Cons r rs => (rosePreord r) @ (mapRosePreord rs)
-end
-termination_by
-  rosePreord => sizeOf R
-  mapRosePreord => sizeOf rs
+  empty := List.Nil
+  enq x Q := x::Q
+  deq q :=
+    match q with
+      | List.Nil => Option.NONE
+      | List.Cons x xs => Option.SOME x
+
+def QueueStacks : Queue α where
+  t := List α
+
+  empty := List.Nil
+  enq x Q := x::Q
+  deq q :=
+    match q with
+      | List.Nil => Option.NONE
+      | List.Cons x xs => Option.SOME x
+
+-- Questions to ask patrick
+  -- importing mathlib
+  -- what should be the type below Bool or Prop??? Equality type????? Tuples?
+
+def isEq {α : Type} (eq : α → α → Bool) : Prop :=
+  (∀ a, eq a a) ∧
+  (∀ a b, eq a b ↔ eq b a) ∧
+  (∀ a b c, (eq a b) ∧ (eq b c) → eq a c)
+
+def queueQuiv {α : Type} (Q : Queue α) (Q' : Queue α) (eq : α → α → Bool) (fxnEq : isEq eq) : Prop :=
+  ∃ R : Q.t -> Q'.t -> Prop,
+    (R (Q.empty) (Q'.empty)) ∧
+    (∀ q q' x, R q q' → R (Q.enq x q) (Q'.enq x q')) ∧
+    (∀ q q', R q q' →
+      (∃ x, Q.deq q = Option.SOME x → ∃ y, Q'.deq q' = Option.SOME y ∧ eq x y) ∧
+      (Q.deq q = Option.NONE → Q'.deq q' = Option.NONE))
+
+theorem stackListRI {α : Type} (eq : α → α → Bool) (fxnEq : isEq eq) : queueQuiv QueueList QueueStacks eq fxnEq :=
+  by sorry
 
 end structural_datatypes
